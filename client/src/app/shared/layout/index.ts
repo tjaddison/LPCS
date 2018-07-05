442728355ab3365d02861dfae9c6345b242e8334
Terrance Addison <terrance@Terrances-MBP.hsd1.va.comcast.net>
2018-07-05 18:38:59 -0400

    massive commit

--- client/src/app/shared/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './footer.component';
-export * from './nav-bar.component';
-export * from './right-sidebar.component';
--- client/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  api_url: 'http://localhost:15354/api',
-  clientID: 'cd7588e7-f875-4172-b66f-48d63307cb2d',
-  graphScopes: ['openid'],
-  signUpSignInPolicy: 'B2C_1_SiUpIn',
-  tenant: 'b2cvhda.onmicrosoft.com'
-};
